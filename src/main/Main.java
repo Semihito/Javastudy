4d78944911df643aa19012653b48267e3520e037
SEMIHITOPC\Administrator <kamushin.1.aniota@gmail.com>
2018-08-08 00:27:18 +0900

    新規作成

---
+++ src/main/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * せみひと
+ */
+public class Main {
+    public static void main(String[] args) {
+        boolean tenki = false;
+        if(tenki == true) {
+            System.out.println("洗濯をします");
+            System.out.println("散歩に行きます");
+        }else{
+            System.out.println("DVDを見ます");
+        }
+    }
+    
+}
