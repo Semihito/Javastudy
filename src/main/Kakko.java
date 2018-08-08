86bd81d74b39a517fb57868cf8dc15049fe8798c
SEMIHITOPC\Administrator <kamushin.1.aniota@gmail.com>
2018-08-09 02:27:35 +0900

    新規付け加え

---
+++ src/main/Kakko.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Kakko {
+    public static void main(String[] args){      //mainに合わせる
+        boolean tenki =true;
+        if (tenki == true){
+            System.out.println("洗濯をします");
+            System.out.println("散歩に行きます");
+        }else
+            System.out.println("DVDを見ます");
+    }
+}
